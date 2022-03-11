89b8a900667e5762946987973a64b2b053b9b29d
Ryan Cabell <cabell.ryan@gmail.com>
2022-03-10 21:31:35 -0500

    Added functionality to browse button

--- aiart.py
+++ aiart.py
@@ -31,52 +31,63 @@ class Frame:
 
         # Create a photoimage object of the image in the path
         filepath = "./res/test.jpg"
-        self.image = PIL.Image.open(filepath)
-        self.tk_image = PIL.ImageTk.PhotoImage(self.image)
+        self.image_bg = PIL.Image.open(filepath)
+        self.tk_image_bg = PIL.ImageTk.PhotoImage(self.image_bg)
         filepath2 = "./res/car.jpg"
-        self.image2 = PIL.Image.open(filepath2)
-        self.tk_image2 = PIL.ImageTk.PhotoImage(self.image2)
+        self.user_image = PIL.Image.open(filepath2)
+        self.tk_user_image = PIL.ImageTk.PhotoImage(self.user_image)
 
         # Position image on label
-        self.label1 = tk.Label(image=self.tk_image)
-        self.label1.place(x=0, y=0)
+        self.image_bg_label = tk.Label(image=self.tk_image_bg)
+        self.image_bg_label.place(x=0, y=0)
 
         # Center text label
-        Label(self.root, text='PIL Display', font=(
+        Label(self.root, text='AiArt', font=(
             'Helvetica', 18)).pack(side=TOP, padx=20, pady=30)
 
-        # Functionality buttons
+        ####################### Functionality buttons #######################
         # Refresh
         refreshBtn = tk.Button(self.root, text="Refresh",
-                  command=self.refreshImage(filepath))
+                               command=self.refreshImage)
         refreshBtn.place(x=50, y=100)
 
         # Open new picture
         chooseImageBtn = tk.Button(self.root, text="Browse...",
-                  command=self.chooseImage())
+                                   command=self.chooseImage)
         chooseImageBtn.place(x=50, y=150)
+
         # Remove picture
-        tk.Button(self.root, text="Delete",).place(x=50, y=200)
+        # todo: add command to remove picture
+        removeImageBtn = tk.Button(self.root, text="Delete",)
+        removeImageBtn.place(x=50, y=200)
+
         # Find color palette
-        tk.Button(self.root, text="Find color palette", command=pd.Edit(
-        ).findColorPalette(5, self.image)).place(x=50, y=250)
+        findPaletteBtn = tk.Button(self.root, text="Find color palette", command=lambda: pd.Edit(
+        ).findColorPalette(5, self.user_image))
+        findPaletteBtn.place(x=50, y=250)
+
+        # Exit Program
+        exitBtn = tk.Button(self.root, text="Exit Program",
+                            command=self.root.quit)
+        exitBtn.place(x=50, y=300)
+
+        #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
 
         # Canvas with target image
-        c2 = tk.Canvas(width=CANVAS_X, height=CANVAS_Y, background='gray85')
-        self.image = self.image.resize(
+        global user_canvas
+        user_canvas = tk.Canvas(
+            width=CANVAS_X, height=CANVAS_Y, background='gray85')
+        self.image_bg = self.image_bg.resize(
             (CANVAS_X, CANVAS_Y), PIL.Image.ANTIALIAS)
-        c2.place(relx=0.5, rely=0.5, anchor=W)
-        c2.create_image(self.image.width/2, 0,
-                        image=self.tk_image2, anchor="center")
+        user_canvas.place(relx=0.5, rely=0.5, anchor=W)
+        user_canvas.create_image(self.image_bg.width/2, 0,
+                                 image=self.tk_user_image, anchor="center")
 
-    # Start method
-    def main(self):
-        self.root.mainloop()
-
-        #print(findColorPalette(5, image))
+    ####################### Button Command Methods #######################
 
     # Reset the image back to original
-    def refreshImage(self, filepath):
+    # Todo: relaod image from filepath
+    def refreshImage(self):
         return 0
         img2 = tk.PhotoImage(PIL.Image.open("./res/test.jpg"))
         # ERROR __str__ returned non-string (type JpegImageFile)
@@ -88,16 +99,25 @@ class Frame:
     def chooseImage(self):
         filetypes = (
             ('PNG files', '*.png'),
-            ('JPG files', '*.jpg'),
-            ('Jpeg files', '*.jpeg'))
+            ('JPG files', '*.jpg, *.jpeg'),
+            ('All files', '*'))
 
         filename = fd.askopenfilename(
             title='Select an image',
-            initialdir='/',
+            initialdir='./res',
             filetypes=filetypes)
-
         print(filename)
+        self.user_image = PIL.Image.open(filename)
+        self.tk_user_image = PIL.ImageTk.PhotoImage(self.user_image)
+        user_canvas.create_image(self.image_bg.width/2, 0,
+                                 image=self.tk_user_image, anchor="center")
         return 0
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+
+    # Start method
+    def main(self):
+        self.root.mainloop()
 
 
 # Run Program
@@ -112,6 +132,6 @@ if __name__ == "__main__":
  #   canvas.pack(side="right", fill="none", expand=TRUE)
  #   canvas.create_image(image.width/2, 0, image=tk_image, anchor=N)
 
- #   c2 = tk.Canvas(width=500, height=500, background='gray85')
- #   c2.place(relx=0.5, rely=0.5, anchor=W)
- #   c2.create_image(image.width/2, 0, image=tk_image2, anchor="center")
+ #   user_canvas = tk.Canvas(width=500, height=500, background='gray85')
+ #   user_canvas.place(relx=0.5, rely=0.5, anchor=W)
+ #   user_canvas.create_image(image.width/2, 0, image=tk_image2, anchor="center")
--- pdemon.py
+++ pdemon.py
@@ -9,6 +9,9 @@ class Edit:
     # Generate 5 most common colors in the image
     @staticmethod
     def findColorPalette(num, image):
+        # default value for num = 5
+        if (num < 0):
+            num = 5
         from collections import defaultdict
         pallete = defaultdict(int)
         for pixel in image.getdata():
